0fd8d09c76897e159c6d17f93f4ca27c944e156c
v_dongyang05 <v_dongyang05@172.18.75.211>
2025-10-10 16:50:34 +0800

    IU-2025.2.1 <v_dongyang05@MacBook-Pro.local Create Entity.java

---
+++ fileTemplates/Entity.java
@@ -0,0 +1,16 @@
+import com.baidu.bailing.lib.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ${NAME} extends BaseEntity {
+    
+
+}
